e46d80677fc99757d0f2a05bb29c02c93c166bb8
Yossef Sayed <yousifsayed338@gmail.com>
2025-10-06 23:16:00 +0300

    All files Added by Yossef Sayed

---
+++ test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    struct B
+    {
+        char c1;
+        char c2;
+        int n;
+    };
+
+    struct D
+    {
+        char x;
+        struct B y;
+        float z;
+    };
+
+    printf("size of structure : %d", sizeof(struct D));
+    
+}
